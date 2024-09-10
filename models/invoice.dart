ef83f67b9e1a5436c012c7426b486025828fe8b0
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-09-10 14:45:02 +0200

    Added "paid" to Invoice

--- models/invoice.dart
+++ models/invoice.dart
@@ -4,6 +4,7 @@ class Invoice {
   double itemPrice;
   double quantity;
   double taxPercent = 19;
+  bool paid = false;
 
   // Data not required (but nice to have):
   // String articleWhich;
@@ -12,5 +13,6 @@ class Invoice {
   Invoice(
       {required this.itemPrice,
       required this.quantity,
-      required this.taxPercent});
+      required this.taxPercent,
+      required this.paid,});
 }
