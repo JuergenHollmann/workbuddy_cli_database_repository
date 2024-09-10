14a83c292643ecbd360fb651686414f38119f13e
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-09-10 14:29:32 +0200

    Added code to class Invoice

--- models/invoice.dart
+++ models/invoice.dart
@@ -1,8 +1,14 @@
 // invoice.dart
 class Invoice {
+  // Data required:
   double itemPrice;
   double quantity;
   double taxPercent = 19;
+
+  // Data not required (but nice to have):
+  // String articleWhich;
+  // String articleWhere;
+
   Invoice(
       {required this.itemPrice,
       required this.quantity,
