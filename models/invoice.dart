b397c17d65de5cadcf331657b31847c114e656c0
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-09-10 14:50:07 +0200

    Added "invoiceNumber" and "invoiceDate" to Invoice

--- models/invoice.dart
+++ models/invoice.dart
@@ -5,14 +5,19 @@ class Invoice {
   double quantity;
   double taxPercent = 19;
   bool paid = false;
+  String invoiceNumber;
+  String invoiceDate;
 
   // Data not required (but nice to have):
   // String articleWhich;
   // String articleWhere;
 
-  Invoice(
-      {required this.itemPrice,
-      required this.quantity,
-      required this.taxPercent,
-      required this.paid,});
+  Invoice({
+    required this.itemPrice,
+    required this.quantity,
+    required this.taxPercent,
+    required this.paid,
+    required this.invoiceNumber,
+    required this.invoiceDate,
+  });
 }
